63d397328c8148eca8891872b581795191ff44ed
Luis Ángel Méndez Gort <gort.andres000@gmail.com>
2017-11-11 17:42:21 -0500

    Converts to mkv and to h264 if needed

--- main.go
+++ main.go
@@ -3,29 +3,40 @@ package main
 import (
 	"bufio"
 	"flag"
+	"fmt"
 	"os"
 	"os/exec"
 	"path"
 )
 
+const mkv = ".mkv"
+
 func main() {
 	var dest string
 	flag.StringVar(&dest, "d", "", "Destination folder")
 	flag.Parse()
-	s, r := bufio.NewScanner(os.Stdin), make([]string, 0)
-	for s.Scan() {
-		r = append(r, s.Text())
-	}
-
-	for _, j := range r {
-
-		cmd := exec.Command("ffmpeg",
-			"-i", j,
-			"-acodec", "libvorbis",
-			"-vcodec", "copy",
-			path.Join(dest, j),
-		)
-		cmd.Stderr, cmd.Stdout= os.Stderr, os.Stdout
-		cmd.Run()
+	if dest != "." {
+		s, r := bufio.NewScanner(os.Stdin), make([]string, 0)
+		for s.Scan() {
+			r = append(r, s.Text())
+		}
+		acodec := "libvorbis"
+		for _, j := range r {
+			vcodec := "copy"
+			fl, ext := j, path.Ext(j)
+			if ext != mkv {
+				fl, vcodec = j[:len(j)-len(ext)]+mkv, "h264"
+			}
+			cmd := exec.Command("ffmpeg",
+				"-i", j,
+				"-acodec", acodec,
+				"-vcodec", vcodec,
+				path.Join(dest, fl),
+			)
+			cmd.Stderr, cmd.Stdout = os.Stderr, os.Stdout
+			cmd.Run()
+		}
+	} else {
+		fmt.Fprintf(os.Stderr, "Output directory cannot be .\n")
 	}
 }
