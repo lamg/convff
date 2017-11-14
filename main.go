26b079dcfcdf46ba4e802b565c07655100d79334
Luis Ángel Méndez Gort <gort.andres000@gmail.com>
2017-11-14 10:48:05 -0500

    More general

--- main.go
+++ main.go
@@ -9,32 +9,44 @@ import (
 	"path"
 )
 
-const mkv = ".mkv"
-
 func main() {
-	var dest string
+	var dest, targ, dext, ac, vc, conv string
 	flag.StringVar(&dest, "d", "", "Destination folder")
+	flag.StringVar(&targ, "t", "", "Target extension")
+	flag.StringVar(&dext, "e", "", "Output extension")
+	flag.StringVar(&ac, "a", "", "Audio codec")
+	flag.StringVar(&vc, "v", "", "Video codec")
+	flag.StringVar(&conv, "c", "avconv", "Converter")
 	flag.Parse()
 	if dest != "." {
 		s, r := bufio.NewScanner(os.Stdin), make([]string, 0)
 		for s.Scan() {
 			r = append(r, s.Text())
 		}
-		acodec := "libvorbis"
 		for _, j := range r {
-			vcodec := "copy"
-			fl, ext := j, path.Ext(j)
-			if ext != mkv {
-				fl, vcodec = j[:len(j)-len(ext)]+mkv, "h264"
+			ext := path.Ext(j)
+			if ext == targ {
+				fl := j[:len(j)-len(ext)] + dext
+				var cmd *exec.Cmd
+				if ac != "" && vc != "" {
+					cmd = exec.Command(conv,
+						"-i", j,
+						"-acodec", ac,
+						"-vcodec", vc,
+						path.Join(dest, fl),
+					)
+				} else if ac != "" {
+					cmd = exec.Command(conv,
+						"-i", j,
+						"-acodec", ac,
+						path.Join(dest, fl),
+					)
+				}
+				if cmd != nil {
+					cmd.Stderr, cmd.Stdout = os.Stderr, os.Stdout
+					cmd.Run()
+				}
 			}
-			cmd := exec.Command("ffmpeg",
-				"-i", j,
-				"-acodec", acodec,
-				"-vcodec", vcodec,
-				path.Join(dest, fl),
-			)
-			cmd.Stderr, cmd.Stdout = os.Stderr, os.Stdout
-			cmd.Run()
 		}
 	} else {
 		fmt.Fprintf(os.Stderr, "Output directory cannot be .\n")
